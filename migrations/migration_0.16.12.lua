aedb88a3943df7c7534e79d71b9bc98faaaa11d5
Oceanel51 <oceanel51@gmail.com>
2018-11-01 09:52:58 +0200

    0.16.29.2 fix some migration file

--- migrations/migration_0.16.12.lua
+++ migrations/migration_0.16.12.lua
@@ -2,7 +2,7 @@ for i, force in pairs(game.forces) do
 	force.recipes["rapeseed-growth-w"].enabled = force.technologies["advanced-farming"].researched
 	force.recipes["rapeseed-growth-c"].enabled = force.technologies["fertilization"].researched
 	force.recipes["rapeseed-growth-f"].enabled = force.technologies["fertilization-2"].researched
-	force.recipes["compost-rapeseed"].enabled = force.technologies["electric-composter"].researched
+	force.recipes["compost-rapeseed"].enabled = force.technologies["fi-electric-composter"].researched
 	force.recipes["fill-canola-oil-barrel"].enabled = force.technologies["fluid-handling"].researched
 	force.recipes["empty-canola-oil-barrel"].enabled = force.technologies["fluid-handling"].researched
 end
