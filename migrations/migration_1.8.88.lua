d521c597a73aa030fa03b070c82812e1b53686af
hayato.iida <hayato.iida.0213@gmail.com>
2019-09-17 21:30:31 +0900

    fix migration script

--- migrations/migration_1.8.88.lua
+++ migrations/migration_1.8.88.lua
@@ -1,14 +1,13 @@
 for _, player in pairs(game.players) do
 	index = player.index
 
-	if player.valid and player.connected then
-		local leftGui = player.gui.left
+	local leftGui = player.gui.left
 		
-		if not leftGui.frame then break end
+	if not leftGui.frame then break end
 
-		leftGui.frame.destroy()
-		figui.create(index, player)
+	leftGui.frame.destroy()
+	figui.create(index, player)
 
-		player.print("FI migration_1.8.88: main frame of GUI recreated successful")
-	end
+	player.print("FI migration_1.8.88: main frame of GUI recreated successful")
+
 end
